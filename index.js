23a04a839e5f7e3f9d580f74789aeaa4f2b42a94
Kevinou <sophie-bretzel@laposte.net>
2018-09-17 20:44:25 +0200

    ajout pop up javascript

---
+++ index.js
@@ -0,0 +1,8 @@
+function xxvids() {
+	if (confirm('Hey le site xxxvidsxxx est trop bien. Viens dessus stp please')){
+		document.location.href="https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+	}
+	}
+	
+
+setTimeout(xxvids , 2000);
